42a1dcda786e8292fe90ddb3175a3db02574af55
fabs <faebeee@gmail.com>
2019-08-11 22:04:48 +0200

    Add points

--- src/app.js
+++ src/app.js
@@ -4,29 +4,66 @@ import Lab from '@faebeee/lab-utils';
 
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
-const SENSITIVITY = 0.05;
-const analyser = Microphone();
+const SENSITIVITY = null;
+const analyser = Microphone(1024);
 const frequencyArray = new Float32Array(analyser.frequencyBinCount);
 
 Lab.init(WIDTH, HEIGHT);
-Lab.camera.position.set(0, 0, 100);
-Lab.camera.lookAt(0, 0, 0);
+Lab.camera.position.set(32, 10, 100);
+Lab.camera.lookAt(32, 0, 32);
 createLights();
-const sphere = createObject();
-sphere.position.set(0, 0, 0);
-Lab.scene.add(sphere);
+const object = createObject();
+//sphere.position.set(0, 0, 0);
+Lab.scene.add(object);
 
 Lab.update = () => {
     analyser.getFloatTimeDomainData(frequencyArray);
     const noise = updateSensitivity(frequencyArray, SENSITIVITY);
-    sphere.rotation.y += 0.005;
+    const { geometry } = object;
+
+    for (let i = 0; i < noise.length; i++) {
+        geometry.vertices[i].y = noise[i];
+    }
+
+    geometry.verticesNeedUpdate = true;
 };
 
 
 function createObject() {
-    const geometry = new THREE.BoxGeometry(15, 15, 15);
-    const material = new THREE.MeshPhongMaterial({ color: '#51a9ff' });
-    return new THREE.Mesh(geometry, material);
+    const geometry = new THREE.Geometry();
+    for (let x = 0; x < 32; x++) {
+        for (let z = 0; z < 32; z++) {
+            geometry.vertices.push(new THREE.Vector3(x * 2, 0, z * 2));
+        }
+    }
+    var material = new THREE.PointsMaterial({ color: 0xffffff });
+
+    return new THREE.Points(geometry, material);
+}
+
+function vertexShader() {
+    return `
+    varying vec3 vUv; 
+
+    void main() {
+      vUv = position; 
+
+      vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+      gl_Position = projectionMatrix * modelViewPosition; 
+    }
+  `
+}
+
+function fragmentShader() {
+    return `
+      uniform vec3 colorA; 
+      uniform vec3 colorB; 
+      varying vec3 vUv;
+
+      void main() {
+        gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
+      }
+  `
 }
 
 function createLights() {
